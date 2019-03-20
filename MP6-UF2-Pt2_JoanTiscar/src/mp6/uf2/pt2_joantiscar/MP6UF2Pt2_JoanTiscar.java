4249f6a7ac89183d69c4f67a54e8cabcab4e75be
joantiscar <joantiscar@gmail.com>
2019-03-20 18:44:10 +0100

    Sync

---
+++ MP6-UF2-Pt2_JoanTiscar/src/mp6/uf2/pt2_joantiscar/MP6UF2Pt2_JoanTiscar.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mp6.uf2.pt2_joantiscar;
+
+/**
+ *
+ * @author dios
+ */
+public class MP6UF2Pt2_JoanTiscar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
